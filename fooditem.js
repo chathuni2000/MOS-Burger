a76bb4d7bfa706dc8528c6ae5dbb20f0c1571672
chathuni2000 <ahinsapatabedige@gmail.com>
2024-10-09 08:24:32 -0700

    first commit

---
+++ fooditem.js
@@ -0,0 +1,67 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Bison Burger',
+    category : 'burger',
+    rating : 4.3,
+    price: 850,
+    img: 'images/burger/bison burger.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Butter Burger',
+    category : 'burger',
+    rating : 4.3,
+    price: 550,
+    img: 'images/butter burger.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Cheesy Bacon Burger',
+    category : 'burger',
+    rating : 4.3,
+    price: 580,
+    img: 'images/Cheesy Bacon Burger.png',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Cheesy Beef Burger',
+    category : 'burger',
+    rating : 4.3,
+    price: 915,
+    img: 'images/Cheesy Beef Burger.png',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Chimicchurri Burger',
+    category : 'burger',
+    rating : 4.3,
+    price: 1000,
+    img: 'images/Chimicchurri Burger.png',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Elk Burger',
+    category : 'burger',
+    rating : 4.3,
+    price: 720,
+    img: 'images/Elk Burger.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'French Fry Burger',
+    category : 'burger',
+    rating : 4.3,
+    price: 1200,
+    img: 'images/French Fry Burger.png',
+    quantity: 1
+},
+
+
+export {foodItem};
